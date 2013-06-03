7f44120274fcde4eb3aa653296e9eb915e67142f
Roman Ozana <roman@wikidi.com>
2013-06-03 16:11:54 +0200

    Flatter button

--- buttons/flatter/Flatter.php
+++ buttons/flatter/Flatter.php
@@ -28,13 +28,19 @@ class Flatter implements IButton {
 		add_action('wp_head', array($this, 'head'));
 	}
 
+	public function head() {
+		require_once __DIR__ . '/header.phtml';
+	}
+
+
 	/**
 	 * Return setting HTML
 	 *
 	 * @return mixed
 	 */
 	public function getOptionsForm() {
-		include __DIR__ . '/settings.phtml';
+		require_once __DIR__ . '/header.phtml';
+		require_once __DIR__ . '/settings.phtml';
 	}
 
 	/**
@@ -64,19 +70,99 @@ class Flatter implements IButton {
 		if ($this->isEnable()) require_once __DIR__ . '/button.phtml';
 	}
 
+	/**
+	 * Getting post tags
+	 *
+	 * @return string
+	 */
+	public function getPostTags() {
+		if ($posttags = get_the_tags()) {
+			$tags = array();
+			foreach ($posttags as $tag) {
+				$tags[] = $tag->name;
+			}
+			return join(',', $tags) . ',wordpress';
+		} else {
+			return 'wordpress';
+		}
+	}
+
+	/** @var array */
+	public $categories = array(
+		'text' => 'Text',
+		'images' => 'Images',
+		'video' => 'Video',
+		'audio' => 'Audio',
+		'software' => 'Software',
+		'people' => 'People',
+		'rest' => 'Other',
+	);
+	/** @var array */
+	public $lang = array(
+		'en_GB' => 'English',
+		'sq_AL' => 'Albanian',
+		'ar_DZ' => 'Arabic',
+		'be_BY' => 'Belarusian',
+		'bg_BG' => 'Bulgarian',
+		'ca_ES' => 'Catalan',
+		'zh_CN' => 'Chinese',
+		'hr_HR' => 'Croatian',
+		'cs_CZ' => 'Czech',
+		'da_DK' => 'Danish',
+		'nl_NL' => 'Dutch',
+		'eo_EO' => 'Esperanto',
+		'et_EE' => 'Estonian',
+		'fi_FI' => 'Finnish',
+		'fr_FR' => 'French',
+		'es_GL' => 'Galician',
+		'de_DE' => 'German',
+		'el_GR' => 'Greek',
+		'iw_IL' => 'Hebrew',
+		'hi_IN' => 'Hindi',
+		'hu_HU' => 'Hungarian',
+		'is_IS' => 'Icelandic',
+		'in_ID' => 'Indonesian',
+		'ga_IE' => 'Irish',
+		'it_IT' => 'Italian',
+		'ja_JP' => 'Japanese',
+		'ko_KR' => 'Korean',
+		'lv_LV' => 'Latvian',
+		'lt_LT' => 'Lithuanian',
+		'mk_MK' => 'Macedonian',
+		'ms_MY' => 'Malay',
+		'mt_MT' => 'Maltese',
+		'no_NO' => 'Norwegian',
+		'nn_NO' => 'Nynorsk',
+		'fa_FA' => 'Persian',
+		'pl_PL' => 'Polish',
+		'pt_PT' => 'Portuguese',
+		'ro_RO' => 'Romanian',
+		'ru_RU' => 'Russian',
+		'sr_RS' => 'Serbian',
+		'sk_SK' => 'Slovak',
+		'sl_SI' => 'Slovenian',
+		'es_ES' => 'Spanish',
+		'sv_SE' => 'Swedish',
+		'th_TH' => 'Thai',
+		'tr_TR' => 'Turkish',
+		'uk_UA' => 'Ukrainian',
+		'vi_VN' => 'Vietnamese',
+	);
+
 }
 
 /**
  *
  * @property string $enable
- * @property string $width
- * @property string $show_faces
- * @property string $send
- * @property string $layout
- * @property string $action
- * @property string $colorscheme
- * @property string $font
+ * @property string $mode
+ * @property int $https
+ * @property int $popout
+ * @property string $uid
+ * @property string $button
  * @property string $lang
+ * @property string $category
+ * @property string $tags
+ * @property string $description
  *
  * @author Roman Ožana <ozana@omdesign.cz>
  */
@@ -84,13 +170,14 @@ class Options extends \om\Options {
 
 	protected $options = array(
 		'enable' => true,
-		'width' => 450,
-		'show_faces' => false,
-		'send' => false, // send button
-		'layout' => 'button_count', // standard, button_count, box_count
-		'action' => '', // like or recommend
-		'colorscheme' => '', // light or dark
-		'lang' => 'en_US',
-		'font' => '', // font
+		'mode' => 'manual',
+		'https' => 0,
+		'popout' => 1,
+		'uid' => null,
+		'button' => null,
+		'lang' => 'en_GB',
+		'category' => 'text',
+		'description' => '',
+		'tags' => '',
 	);
 }
